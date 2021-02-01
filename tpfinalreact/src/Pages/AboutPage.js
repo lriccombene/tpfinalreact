9478fd83eb0b25c3db34921ba2fd66e958754ebc
tatilumate <lriccombene@gmail.com>
2021-01-31 21:23:17 -0300

    contacto y about

--- tpfinalreact/src/Pages/AboutPage.js
+++ tpfinalreact/src/Pages/AboutPage.js
@@ -1,16 +1,65 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import '../style.css';
+import firebase from "firebase";
 
-class AboutPage extends React.Component{
-    render(){
+function AboutPage (){
+
+    const [image,setImage] = useState([]);
+
+    useEffect(
+        () => {
+
+            async function getImage  (e) {
+                //const conte = useContext(NetContext);
+                //this.state.context=conte;
+
+                // console.log(this.props.product.images.filename)
+                // console.log( this.props.product.images.filename )
+                const ruta = '/utnimages/almacen.jpeg'
+                const storageRef = firebase.storage().ref()
+
+                storageRef.child(ruta).getDownloadURL()
+                    .then((url) => {
+                        // `url` is the download URL for 'images/stars.jpg'
+
+                        // This can be downloaded directly:
+                        var xhr = new XMLHttpRequest();
+                        xhr.responseType = 'blob';
+                        xhr.onload = (event) => {
+                            var blob = xhr.response;
+                        };
+                        xhr.open('GET', url);
+                        xhr.send();
+
+
+                        // Or inserted into an <img> element
+                        var img = document.getElementById('myimagen');
+                        img.setAttribute('src', url);
+                        setImage(url);
+
+                    })
+                    .catch((error) => {
+                        // Handle any errors
+                    });
+            }
+            getImage();
+        }, []);
+
+
         return (
             <div id="wrapper">
 
-                <div>About Estamos en Login</div>
+                <div class="row justify-content-center"><h1>Somos un Almacen de barrio  atendido por sus due;o</h1></div>
+                    <div class="row justify-content-center" ><h3>Estamos en el barrio como vecinos hace mas de 60 a;os</h3></div>
+                <div className="row justify-content-center"><h3>Nos interesa que lleguen los productos mas frescos y de
+                    calidad a nuestros clientes</h3></div>
+                        <div class="row justify-content-center"><h3>somos fan de san lorenzo desde la cuna</h3></div>
+                <div className="image row justify-content-center"><img id='myimagen' src={image} width="424" height="300" alt=""/></div>
+
             </div>
         )
     }
 
-}
+
 
 export default AboutPage
--- tpfinalreact/src/Pages/ContactPage.js
+++ tpfinalreact/src/Pages/ContactPage.js
@@ -2,13 +2,105 @@ import React from 'react'
 import '../style.css';
 
 class ContactPage extends React.Component{
+
+    // inicializamos nuestro estado inicial
+    constructor(props) {
+        super(props);
+        this.state = {
+            form: [],
+            alert: false,
+            alertData: {}
+        };
+    }
+    showAlert(type, message) {
+        this.setState({
+            alert: true,
+            alertData: { type, message }
+        });
+        setTimeout(() => {
+            this.setState({ alert: false });
+        }, 4000)
+    }
+
+    // Con esta funcion borramos todos los elementos del formulario
+    resetForm() {
+        this.refs.contactForm.reset();
+    }
+
+    // Funcion para enviar la informacion del formulario a Firebase Database
+    sendMessage(e) {
+        // Detiene la acción predeterminada del elemento
+        e.preventDefault();
+
+        // Creamos un objeto con los valores obtenidos de los inputs
+        const params = {
+            name: this.inputName.value,
+            email: this.inputEmail.value,
+            city: this.inputCity.value,
+            phone: this.inputPhone.value,
+            message: this.textAreaMessage.value
+        };
+
+        // Validamos que no se encuentren vacios los principales elementos de nuestro formulario
+        if (params.name && params.email && params.phone && params.phone && params.message) {
+            // enviamos nuestro objeto "params" a firebase database
+
+            // limpiamos nuestro formulario llamando la funcion resetform
+            this.resetForm();
+        } else {
+            // En caso de no llenar los elementos necesario despliega un mensaje de alerta
+            this.showAlert('warning', 'Please fill the form');
+        };
+    }
+
     render(){
         return (
-            <div id="wrapper">
+            <div>
 
-                <div>Contact Estamos en Login</div>
+                <div className='container' style={{ padding: `40px 0px` }}>
+                    <div className='row'>
+                        <div className='col-sm-4'>
+                            <h2>Formulario de Contacto</h2>
+                            <form onSubmit={this.sendMessage.bind(this)} ref='contactForm' >
+                                <div className='form-group'>
+                                    <label htmlFor='name'>Nombre</label>
+                                    <input type='text' className='form-control' id='name'
+                                           placeholder='Name' ref={name => this.inputName = name}
+                                    />
+                                </div>
+                                <div className='form-group'>
+                                    <label htmlFor='exampleInputEmail1'>Email</label>
+                                    <input type='email' className='form-control' id='email'
+                                           placeholder='Email' ref={email => this.inputEmail = email}
+                                    />
+                                </div>
+                                <div className='form-group'>
+                                    <label htmlFor='city'>Ciudad</label>
+                                    <select className='form-control' id='city' ref={city => this.inputCity = city}>
+                                        <option value='Bs As'>Bs AS</option>
+                                        <option value='CABA'>CABA</option>
+                                        <option value='Mar del Plata'>Mar del Plata</option>
+                                    </select>
+                                </div>
+                                <div className='form-group'>
+                                    <label htmlFor='phone'>Telefono</label>
+                                    <input type='number' className='form-control' id='phone'
+                                           placeholder='+52 1' ref={phone => this.inputPhone = phone}
+                                    />
+                                </div>
+                                <div className='form-group'>
+                                    <label htmlFor='message'>Mensaje</label>
+                                    <textarea className='form-control' id='message'
+                                              rows='3' ref={message => this.textAreaMessage = message}>
+                  </textarea>
+                                </div>
+                                <button type='submit' className='btn btn-primary'>Enviar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
 
 }
